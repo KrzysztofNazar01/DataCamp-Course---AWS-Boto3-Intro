f99e89a56c9df6b61f375f85fec15ae14342f959
Krzysztof Nazar <s184698@student.pg.edu.pl>
2023-10-08 10:39:44 +0200

    Get s3 buckets and sns topics

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import os
+from dotenv import load_dotenv
+import boto3
+import json
+from datetime import datetime
+from dateutil import tz
 
 
-# Press the green button in the gutter to run the script.
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            # Convert datetime objects to ISO 8601 format strings
+            return obj.isoformat()
+        return super().default(obj)
+
+
+def print_formatted_json(data):
+    # Convert the dictionary to a pretty JSON string
+    formatted_json = json.dumps(data, indent=4, cls=DateTimeEncoder)
+    print(formatted_json)
+
+
+def get_s3_buckets(AWS_KEY_ID, AWS_SECRET, aws_region):
+    load_dotenv()
+    s3 = boto3.client('s3',
+                      region_name=aws_region,
+                      aws_access_key_id=AWS_KEY_ID,
+                      aws_secret_access_key=AWS_SECRET)
+    resposne = s3.list_buckets()
+
+    print_formatted_json(resposne)
+
+
+def get_sns_topics(AWS_KEY_ID, AWS_SECRET, aws_region):
+    # Generate the boto3 client for interacting with S3 and SNS
+    s3 = boto3.client('s3', region_name=aws_region,
+                      aws_access_key_id=AWS_KEY_ID,
+                      aws_secret_access_key=AWS_SECRET)
+
+    sns = boto3.client('sns', region_name=aws_region,
+                       aws_access_key_id=AWS_KEY_ID,
+                       aws_secret_access_key=AWS_SECRET)
+
+    # List S3 buckets and SNS topics
+    buckets = s3.list_buckets()
+    topics = sns.list_topics()
+
+    # Print out the list of SNS topics
+    print_formatted_json(topics)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    load_dotenv()
+    AWS_KEY_ID = os.getenv('AWS_KEY_ID')
+    AWS_SECRET = os.getenv('AWS_SECRET')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    get_s3_buckets(AWS_KEY_ID, AWS_SECRET, 'eu-central-1')
+    get_sns_topics(AWS_KEY_ID, AWS_SECRET, 'eu-central-1')
